09189f7e2ebcbfc50f9b0687f74d91382320cbe7
Ruslan Tverezenko <68191748+ruslantverezenko@users.noreply.github.com>
2023-01-17 02:17:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppUserProfile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppUserProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
