4e29a99d8f389693db6857fe03953fbe14d04422
Ruslan Tverezenko <68191748+ruslantverezenko@users.noreply.github.com>
2023-01-17 16:05:41 +0300

    Finished the program

--- ConsoleAppUserProfile/Program.cs
+++ ConsoleAppUserProfile/Program.cs
@@ -10,6 +10,48 @@ namespace ConsoleAppUserProfile
     {
         static void Main(string[] args)
         {
+            for (; ; )
+            {
+                Mandarin();
+                Console.Write("Enter the data again(y = yes | n = no): ");
+                string userSelection = Console.ReadLine();
+                if (userSelection == "y" ||  userSelection == "yes")
+                {
+                    continue;
+                    Console.Clear();
+                }
+                if (userSelection == "n" || userSelection == "no")
+                {
+                    break;
+                }
+            }
+        }
+        static void Mandarin()
+        {
+            Console.Write("Enter your last name: ");
+            string lastNameUser = Console.ReadLine();
+
+            Console.Write("Enter a name: ");
+            string nameUser = Console.ReadLine();
+
+            Console.Write("Enter the age: ");
+            int ageUser = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the weight (in kilograms): ");
+            double weightUser = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter height in meters(for example 1,78): ");
+            double heightUser = double.Parse(Console.ReadLine());
+
+            double bodyMassIndexUser = weightUser / (heightUser * heightUser);
+            string userProfile = "Profile\n"
+                + $"Full name: {lastNameUser} {nameUser}\n"
+                + $"Age: {ageUser}\n"
+                + $"Weight: {weightUser}\n"
+                + $"Height: {heightUser}\n"
+                + $"Body Mass Index: {bodyMassIndexUser}";
+
+            Console.WriteLine(userProfile);
         }
     }
 }
